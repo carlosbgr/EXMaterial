62b0a43f595df1a6be51884f1aa2023560226e12
Carlos Balaguer <balaguer.88@gmail.com>
2019-07-22 23:37:57 +0200

    Add Translate and Toastr
    Add Translate service and Toastr service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'EXMaterial';
+
+  languages: any[] = [];
+
+  constructor(
+    private _translateService: TranslateService,
+    private _http: HttpClient,
+    private _toastr: ToastrService,
+    ) {
+    this._translateService.setDefaultLang('es');
+    this.getJSON('../assets/i18n/languages.json').subscribe((lan: any) => {
+      for (let i = 0 ; i < lan.length; i += 1) {
+        this.languages.push(lan[i]);
+      }
+    });
+  }
+
+  public getJSON(json: string): Observable<any> {
+    return this._http.get(json);
+  }
+
+  public switchLanguage(event: any) {
+    this._translateService.use(event.target.selectedOptions[0].id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { NgModule } from '@angular/core';
-import { CustomMaterialModule } from './core/material.module';
-import { AppRoutingModule } from './core/app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { CustomMaterialModule } from './core/material.module';
+import { AppRoutingModule } from './core/app-routing.module';
+
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppComponent } from './app.component';
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -18,6 +27,19 @@ import { AppComponent } from './app.component';
     CustomMaterialModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
